3c557f1ab2d4acbbe34cc84e8f258974652f6db7
Kristijonas Kuzmickas <96790345+kkuzmickas@users.noreply.github.com>
2025-11-16 20:51:08 +0200

    Refactor MeteoLT integration setup and unload methods

--- custom_components/meteo_lt/__init__.py
+++ custom_components/meteo_lt/__init__.py
@@ -1,29 +1,19 @@
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
-from .api import MeteoLTApi
-from .coordinator import MeteoCoordinator
-
+from .coordinator import MeteoLtDataUpdateCoordinator
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-session = async_get_clientsession(hass)
-api = MeteoLTApi(session, entry.data["place"])
+    coordinator = MeteoLtDataUpdateCoordinator(hass, entry.data)
+    await coordinator.async_config_entry_first_refresh()
 
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-coordinator = MeteoCoordinator(hass, api)
-await coordinator.async_config_entry_first_refresh()
-
-
-hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-
-await hass.config_entries.async_forward_entry_setups(entry, ["weather"])
-return True
-
+    # Forward to sensor platform
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "weather")
-if unload_ok:
-hass.data[DOMAIN].pop(entry.entry_id)
-return unload_ok
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
