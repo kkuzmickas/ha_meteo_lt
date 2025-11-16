d27f38349607355bc8b842c9c9d41386ebeece26
Kristijonas Kuzmickas <96790345+kkuzmickas@users.noreply.github.com>
2025-11-16 20:41:52 +0200

    Refactor MeteoLTWeather to MeteoWeather class

--- custom_components/meteo_lt/weather.py
+++ custom_components/meteo_lt/weather.py
@@ -1,50 +1,67 @@
-from homeassistant.components.weather import (
-    WeatherEntity,
-    WeatherEntityFeature,
-)
+from homeassistant.components.weather import WeatherEntity, WeatherEntityFeature
 from homeassistant.const import TEMP_CELSIUS
 from .const import DOMAIN
 
-ASCII_MAP = {
-    "clear": "sunny",
-    "cloudy": "cloudy",
-    "rain": "rainy",
-    "snow": "snowy",
-    "fog": "fog",
+
+MAP = {
+"clear": "sunny",
+"partly-cloudy": "partlycloudy",
+"cloudy": "cloudy",
+"cloudy-with-sunny-intervals": "partlycloudy",
+"rain": "rainy",
+"light-rain": "rainy",
+"sleet": "snowy-rainy",
+"snow": "snowy",
+"fog": "fog",
+"thunderstorms": "lightning"
 }
 
+
 async def async_setup_entry(hass, entry, async_add_entities):
-    api = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([MeteoLTWeather(api)])
+coordinator = hass.data[DOMAIN][entry.entry_id]
+async_add_entities([MeteoWeather(coordinator)])
 
-class MeteoLTWeather(WeatherEntity):
-    _attr_name = "Meteo.lt Weather"
-    _attr_native_temperature_unit = TEMP_CELSIUS
-    _attr_supported_features = WeatherEntityFeature.FORECAST_HOURLY
 
-    def __init__(self, api):
-        self.api = api
-        self.data = None
+class MeteoWeather(WeatherEntity):
+_attr_native_temperature_unit = TEMP_CELSIUS
+_attr_supported_features = WeatherEntityFeature.FORECAST_HOURLY
 
-    async def async_update(self):
-        self.data = await self.api.get_forecast()
 
-    @property
-    def native_temperature(self):
-        return self.data["forecastTimestamps"][0]["airTemperature"]
+def __init__(self, coordinator):
+self.coordinator = coordinator
 
-    @property
-    def condition(self):
-        code = self.data["forecastTimestamps"][0]["conditionCode"]
-        return ASCII_MAP.get(code, "cloudy")
 
-    @property
-    def forecast(self):
-        return [
-            {
-                "datetime": ts["forecastTimeUtc"],
-                "temperature": ts["airTemperature"],
-                "condition": ASCII_MAP.get(ts["conditionCode"], "cloudy")
-            }
-            for ts in self.data["forecastTimestamps"][:24]
-        ]
+@property
+def name(self):
+return "Meteo.lt Weather"
+
+
+@property
+def native_temperature(self):
+return self._now()["airTemperature"]
+
+
+@property
+def condition(self):
+return MAP.get(self._now()["conditionCode"], "cloudy")
+
+
+def _now(self):
+data = self.coordinator.data["forecastTimestamps"]
+return data[0]
+
+
+@property
+def forecast(self):
+return [
+{
+"datetime": ts["forecastTimeUtc"],
+"temperature": ts["airTemperature"],
+"condition": MAP.get(ts["conditionCode"], "cloudy"),
+}
+for ts in self.coordinator.data["forecastTimestamps"][:24]
+]
+
+
+async def async_update(self):
+await self.coordinator.async_request_refresh()
